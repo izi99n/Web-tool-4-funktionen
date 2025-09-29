ee51f3613cb310dd4445ee80f7bbb06db4ab372a
izi99n <149567459+izi99n@users.noreply.github.com>
2025-09-29 23:36:17 +0200

    Initial commit: alle Projektdateien

---
+++ script.js
@@ -0,0 +1,8 @@
+//von Seite 1 auf Seite 2//
+
+
+document.getElementById("clickBtn").addEventListener("click", () => {
+    window.location.href = "seite2.html";
+});
+
+
---
+++ seite2.js
@@ -0,0 +1,17 @@
+//von Seite 2 auf Seite Game, To-Do, Taschenrechner, Währungsrechner//
+
+document.getElementById("gameBtn").addEventListener("click", () => {
+  window.location.href = "xox.html";
+});
+
+document.getElementById("todoBtn").addEventListener("click", () => {
+  window.location.href = "todo.html";
+});
+
+document.getElementById("calcBtn").addEventListener("click", () => {
+  window.location.href = "calculator.html";
+});
+
+document.getElementById("currencyBtn").addEventListener("click", () => {
+  window.location.href = "currency.html";
+});
